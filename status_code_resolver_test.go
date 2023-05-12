35897ae9ccdd2ebd8fe7b3a08cab2c32c44d14fe
George Kampanos <george.kampanos@nandos.co.uk>
2023-05-12 18:25:49 +0100

    tests

--- status_code_resolver_test.go
+++ status_code_resolver_test.go
@@ -1,5 +1,33 @@
 package main
 
-func ResolveStatusCode_Test(statusCode int) string {
-	return ""
+import (
+	"fmt"
+	"testing"
+)
+
+func TestResolveStatusCode(t *testing.T) {
+	tests := []struct {
+		input         string
+		expected      string
+		wantErr       bool
+		expectedErr error
+	}{
+		{"200", "200: OK", false, nil},
+		{"404", "404: Not Found", false, nil},
+		{"500", "500: Internal Server Error", false, nil},
+		{"foo", "", true, fmt.Errorf("foo is not a valid input")},
+		{"999", "", true, fmt.Errorf("999 is not a valid status code")},
+	}
+
+	for _, tc := range tests {
+		result, err := ResolveStatusCode(tc.input)
+
+		if result != tc.expected {
+			t.Errorf("input %s, expected %s, got %s", tc.input, tc.expected, result)
+		}
+
+		if tc.wantErr && (tc.expectedErr != err) {
+			t.Errorf("input %s, expected %s, got %s", tc.input, tc.expectedErr.Error(), err.Error())
+		}
+	}
 }
